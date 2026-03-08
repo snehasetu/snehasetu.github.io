e0c3caafeb5cdedf33453e1a035beccba4cc5150
snehasetu <snehasetu@protonmail.com>
2026-03-08 20:33:36 +0530

    Create db.ts

---
+++ server/db.ts
@@ -0,0 +1,8 @@
+import { drizzle } from 'drizzle-orm/postgres-js';
+import postgres from 'postgres';
+import * as schema from '../shared/schema';
+
+const connectionString = process.env.DATABASE_URL!;
+
+const client = postgres(connectionString, { ssl: 'require' });
+export const db = drizzle(client, { schema });
